9404df86cfa238fbcc86fe6c8fc142b2b282df37
koga3z <ytleejianxhe@gmail.com>
2024-08-03 21:20:39 +0800

    Revert "Merge branch 'jiayi/develop' into eric/develop"
    This reverts commit 59b466259bc24bec79f814d2410a8c22bd9597ce, reversing
    changes made to 32836dbf54d2f1554322c8081356ff79a995801a.

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,10 @@
-import 'package:calories_tracking/core/utils/time_parser.dart';
+import 'package:calories_tracking/features/coach_register.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:calories_tracking/core/theme/app_theme.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:calories_tracking/features/user_main/screens/user_main_screen.dart';
-import 'package:calories_tracking/features/user_main/bloc/quest_bloc.dart';
-import 'package:calories_tracking/features/user_main/bloc/user_bloc.dart';
-import 'package:calories_tracking/features/user_main/repositories/quest_repository.dart';
-import 'package:calories_tracking/features/user_main/repositories/user_repository.dart';
-import 'package:calories_tracking/features/book_coaches/repositories/coach_repository.dart';
-import 'package:calories_tracking/features/workouts/repositories/workout_repository.dart';
 
-import 'features/admin_main/screens/admin_main_screen.dart';
+import 'core/theme/app_theme.dart';
+import 'features/coach_main/screens/coach_homepage.dart';
 
-//TODO remove print statements used for terminal logging, non production purposes only
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  final malaysiaTime = TimeParser.getMalaysiaTime();
-  print('App started at (Malaysia Time - MYT):');
-  print(TimeParser.formatDateTime(malaysiaTime));
-
+void main() {
   runApp(const MyApp());
 }
 
@@ -29,40 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiRepositoryProvider(
-      providers: [
-        RepositoryProvider<CoachRepository>(
-          create: (context) => CoachRepository(),
-        ),
-        RepositoryProvider<WorkoutRepository>(
-          create: (context) => WorkoutRepository(),
-        ),
-        RepositoryProvider<UserRepository>(
-          create: (context) => UserRepository(),
-        ),
-        RepositoryProvider<QuestRepository>(
-          create: (context) => QuestRepository(),
-        ),
-      ],
-      child: MultiBlocProvider(
-        providers: [
-          BlocProvider<UserBloc>(
-            create: (context) => UserBloc(context.read<UserRepository>()),
-          ),
-          BlocProvider<QuestBloc>(
-            create: (context) => QuestBloc(
-              context.read<QuestRepository>(),
-              context.read<UserBloc>(),
-            ),
-          ),
-        ],
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'FitQuest',
-          theme: AppTheme.lightTheme,
-          home: const AdminMainScreen(),
-        ),
-      ),
+    return MaterialApp(
+      title: 'FitQuest',
+      theme: AppTheme.lightTheme,
+      home: const CoachRegister(),
     );
   }
 }
